322f4887ddb532a37ef8dd951c933c25febdbabe
Evgeniy Lopatenko <e.lopatenko@gmail.com>
2016-04-18 14:53:28 +0300

    change gcm url

--- src/sm_gcm_api.erl
+++ src/sm_gcm_api.erl
@@ -1,7 +1,7 @@
 -module(sm_gcm_api).
 -export([push/3]).
 
--define(BASEURL, "http://android.googleapis.com/gcm/send").
+-define(BASEURL, "https://gcm-http.googleapis.com/gcm/send").
 
 -type header()  :: {string(), string()}.
 -type headers() :: [header(),...].
